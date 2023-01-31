d215e2c0a4de83dd06482425a4eb41e75ff34f7e
King Kong <king-kong@gmail.com>
2023-01-31 11:06:53 -0800

    charts route displays users gained

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { useState } from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
+import { UserData } from './ChartData.js';
 import './App.css';
+import BarChart from './components/barChart.js';
 
 function App() {
+  const [userData] = useState({
+    labels: UserData.map((data) => data.year),
+    datasets: [
+      {
+        label: 'Users Gained',
+        data: UserData.map((data) => data.userGain),
+      },
+    ],
+  });
   return (
     <div className="App">
       <nav>
@@ -14,6 +26,7 @@ function App() {
         </Route>
         <Route path="/charts">
           <h1>Chart Library</h1>
+          <BarChart chartData={userData} />
         </Route>
       </Switch>
     </div>
